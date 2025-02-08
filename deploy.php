55aff9ce941fd99ee266a247ef96acb20c610389
Giancarlo Di Massa <giancarlo@digitall.it>
2025-02-08 20:07:15 +0100

    Make SQLite database shared between releases

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/laravel.php';
 
 set('repository', 'https://github.com/digitall-it/archivio.london.git');
 
-add('shared_files', []);
+add('shared_files', ['database/database.sqlite']);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
