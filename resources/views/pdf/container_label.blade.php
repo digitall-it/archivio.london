8219901f882cd6771d82dfcccce943070633ad4b
Giancarlo Di Massa <giancarlo@digitall.it>
2025-02-09 18:59:47 +0100

    Cosmetic fixes to container label

--- resources/views/pdf/container_label.blade.php
+++ resources/views/pdf/container_label.blade.php
@@ -7,7 +7,7 @@
         }
 
         body {
-            margin: 0;
+            margin: 0mm;
             padding: 0;
             font-family: 'Dosis', sans-serif;
             transform: rotate(90deg);
@@ -26,7 +26,7 @@
         }
 
         td.label {
-            font-size: 16px;
+            font-size: 32px;
             font-weight: bold;
             text-transform: uppercase;
             white-space: nowrap;
@@ -38,10 +38,10 @@
 <table rotate="90">
     <tr>
         <td class="code">
-            <barcode code="{{ url('/qr/' . $mode . '/' . $id) }}" type="QR" size="1" error="M" disableborder="1"/>
+            <barcode code="{{ url('/qr/' . $mode . '/' . $id) }}" type="QR" size="0.8" error="M" disableborder="1"/>
         </td>
         <td class="label">
-            <span>{{ $containerName }} &middot; {{ $mode === 'load' ? 'carica' : 'scarica' }}</span>
+            <span>{{ $containerName }}<br>{{ $mode === 'load' ? 'carica' : 'scarica' }}</span>
         </td>
     </tr>
 </table>
